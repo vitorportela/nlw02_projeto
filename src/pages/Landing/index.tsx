303eafabb64680e1f79e182217feca43f4846405
vitorportela <vitor.portela@live.com>
2020-08-06 00:39:50 -0300

    90% Front-End

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
 import studyIcon from "../../assets/images/icons/study.svg";
@@ -20,14 +22,14 @@ function Landing() {
                      className="hero-image"
                 />
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas"/>
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
                 <span className="total-connections">
                     Total de 200 conexoes já realizadas <img src={purpleHeartIcon} alt="Coração Roxo"/>
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+import './styles.css';
+import PageHeader from '../../Components/PageHeader';
+
+function TeacherList(){
+    return(
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matérias</label>
+                        <input type="text" id="subject"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da semana</label>
+                        <input type="text" id="subject"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="subject"/>
+                    </div>
+
+
+
+                </form>
+            </PageHeader>
+        </div>
+    )
+}
+
+export default TeacherList;
