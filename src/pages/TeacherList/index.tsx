a63067d8673eee36bad8a3835a300eaf4933acd2
vitorportela <vitor.portela@live.com>
2020-08-08 13:45:26 -0300

    retirado pasta vscode

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 
-import './styles.css';
+
 import PageHeader from '../../Components/PageHeader';
 import TeacherItem from '../../Components/TeacherItem';
+import './styles.css';
 
 
 function TeacherList(){
